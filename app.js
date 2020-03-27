5d3da670012bc05e6b8f0d59e0e23b234f2a7ec4
Patrick-web <pntxall100@gmail.com>
2020-03-27 20:06:33 +0300

    add,mark done and delete actions

--- app.js
+++ app.js
@@ -2,16 +2,39 @@ var study = new Vue({
   el: "#vue-app",
   data: {
     heading: "Vue.js",
-    todos:['Do the CAT','Clean up the room']
+    todos:[]
   },
   methods:{
     addTodo(){ 
-      const newTodo = document.querySelector("#todo");
-      
-      this.todos.unshift(newTodo.value);
-      newTodo.value=''
+      const todo = document.querySelector("#todo");
+      const newTodo = {
+        id: Date.now(),
+        todo: todo.value,
+        isDone:false
+      }
+      this.todos.unshift(newTodo);
+      todo.value=''
       console.log(this.todos)
+    },
+    deleteTodo(index){
+      this.todos.splice(index,1);
+    },
+    markAsDone(id){
+      const targetID= this.todos.map(todo=>todo.id).indexOf(id)
+      this.todos[targetID].isDone=!this.todos[targetID].isDone
+      console.log(this.todos[targetID].isDone)
+    },
+    showActions(e){
+      console.log(e.target)
+      let todo;
+      if(e.target.classList.contains('todo')){
+        todo = e.target; 
+      }else if(e.target.classList.contains('todoName')){
+        todo=e.target.parentElement;      
+        
+      }
+      
+      todo.classList.toggle('showActions')
     }
   }
 });
-console.log("This editor is amazing");
